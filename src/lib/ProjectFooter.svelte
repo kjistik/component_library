80a6c71c96963e5b696ca470a19043744bc731bb
kjistik <coronelmartinernesto@gmail.com>
2024-05-02 16:55:03 -0300

    added each block for links

--- src/lib/ProjectFooter.svelte
+++ src/lib/ProjectFooter.svelte
@@ -5,6 +5,7 @@
     export let background_color = "#5b7553";
     export let left_side = "Martín Ernesto Coronel";
     export let center_test = "Derechos reservados etc etc etc";
+
     onMount(() => {
         document.documentElement.style.setProperty("--text-color", text_color);
         document.documentElement.style.setProperty(
@@ -18,30 +19,16 @@
     <p class="left">{left_side}</p>
     <p>{center_test}</p>
     <div class="images">
-        <a href={projectFooter[2].link}
-            ><img
-                src={projectFooter[2].icon}
-                width="28"
-                height="28"
-                alt={projectFooter[2].desc}
-            /></a
-        >
-        <a href={projectFooter[0].link}>
-            <img
-                src={projectFooter[0].icon}
-                width="28"
-                height="28"
-                alt={projectFooter[0].desc}
-            />
-        </a>
-        <a href={projectFooter[1].link}>
-            <img
-                src={projectFooter[1].icon}
-                width="28"
-                height="28"
-                alt={projectFooter[1].desc}
-            />
-        </a>
+        {#each projectFooter as item}
+            <a href={item.link}
+                ><img
+                    src={item.icon}
+                    width="28"
+                    height="28"
+                    alt={item.desc}
+                /></a
+            >
+        {/each}
     </div>
 </div>
 
